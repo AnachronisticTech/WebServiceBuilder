77c7ace379ea4753cad3ae4c7c56982a4096fe33
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2023-07-27 22:18:22 +0300

    Fix for crash when attempting to find Resources directory

--- Sources/WebServiceBuilder/LeafViewProvider.swift
+++ Sources/WebServiceBuilder/LeafViewProvider.swift
@@ -19,7 +19,7 @@ public extension LeafViewProvider
 {
     func setupResourcesDirectoryHierarchy() throws
     {
-        guard let bundleResourcesPath = pathOfFolder(identifiedByFileAtPath: "Resources/resources", withExtension: "txt") else { return }
+        guard let bundleResourcesPath = pathOfFolder(identifiedByFileAtPath: "resources", withExtension: "txt") else { return }
 
         try? FileManager.default.removeItem(atPath: "\(resourcesDirectoryPath)Views/\(resourcesDirectoryPathComponent)")
         try copyContentsOf(directoryAt: bundleResourcesPath + "/Views", to: "\(resourcesDirectoryPath)Views/\(resourcesDirectoryPathComponent)")
