9d0b35718eb0040dd0e27ef419eaa7d70560fc90
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2023-07-28 18:25:37 +0300

    Fix for incorrect resources path on macOS

--- Sources/WebServiceBuilder/LeafViewProvider.swift
+++ Sources/WebServiceBuilder/LeafViewProvider.swift
@@ -19,7 +19,12 @@ public extension LeafViewProvider
 {
     func setupResourcesDirectoryHierarchy() throws
     {
-        guard let bundleResourcesPath = pathOfFolder(identifiedByFileAtPath: "resources", withExtension: "txt") else { return }
+        #if os(macOS)
+        let path = "Resources/resources"
+        #else
+        let path = "resources"
+        #endif
+        guard let bundleResourcesPath = pathOfFolder(identifiedByFileAtPath: path, withExtension: "txt") else { return }
 
         try? FileManager.default.removeItem(atPath: "\(resourcesDirectoryPath)Views/\(resourcesDirectoryPathComponent)")
         try copyContentsOf(directoryAt: bundleResourcesPath + "/Views", to: "\(resourcesDirectoryPath)Views/\(resourcesDirectoryPathComponent)")
--- Sources/WebServiceBuilder/LogBehaviour.swift
+++ Sources/WebServiceBuilder/LogBehaviour.swift
@@ -10,4 +10,19 @@ public enum LogBehaviour : Int
     case none = 0
     case errorsAndWarnings = 1
     case full = 2
+
+    public init(stringValue: String)
+    {
+        self = .none
+        let lower = stringValue.lowercased()
+        if lower == "full"
+        {
+            self = .full
+        }
+        else if lower == "errorsandwarnings" || lower == "errors_and_warnings"
+        {
+            self = .errorsAndWarnings
+        }
+
+    }
 }
